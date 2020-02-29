4431db27b21abbe8548da5cf97f9862e4bcfb0e1
ouxianfei <ouxianfei@smail.nju.edu.cn>
2020-02-29 12:16:41 +0800

    refactor am-test-template

--- rules/am-test.mk
+++ rules/am-test.mk
@@ -1,15 +1,21 @@
 # note INPUT=REF is designed for microbench
 # arg1 dir
 # arg2 name eg. videotest microbench
+# arg3 objdir default: obj/name
+# arg4 env
 define test_template
 $(2)_APP := $(1)/build/$(2)
+ifneq ($(3),)
+$(2)_OBJDIR := $(3)
+else
 $(2)_OBJDIR := $(OBJ_DIR)/$(2)
+endif
 $(2)_DEPS != find $(1) -regex ".*.\(c\|h\|cc\|cpp\|S\)"
 
 .PHONY: compile-$(2) run-$(2) run-nemu-$(2) clean-$(2)
 
 $$($(2)_APP)-$(ARCH).%: $$($(2)_DEPS)
-	@make -s -C $(1) ARCH=$(ARCH)
+	@make -s -C $(1) ARCH=$(ARCH) $(4)
 
 compile-$(2): $$($(2)_APP)-$(ARCH).elf \
 			  $$($(2)_APP)-$(ARCH).bin \
@@ -33,20 +39,3 @@ run-nemu-$(2): $(MIPS32_NEMU) compile-$(2)
 clean-$(2):
 	@make -s -C $(1) ARCH=$(ARCH) clean
 endef
-
-INPUT ?= REF # for microbench, another mode is TEST
-
-AM_TESTS := $(filter-out cputests,$(shell ls $(AM_HOME)/tests))
-AM_APPS := $(shell ls $(AM_HOME)/apps)
-
-# AM apps
-$(foreach app,$(AM_APPS),$(eval $(call test_template,$(AM_HOME)/apps/$(app),$(app))))
-
-# AM tests
-$(foreach app,$(AM_TESTS),$(eval $(call test_template,$(AM_HOME)/tests/$(app),$(app))))
-
-# insttest
-$(eval $(call test_template,$(INSTTEST_HOME),insttest))
-
-# nanos
-$(eval $(call test_template,$(NANOS_HOME),nanos))
--- rules/linux.mk
+++ rules/linux.mk
@@ -27,4 +27,4 @@ run-nemu-u-boot run-nemu-linux: $(EMU_BIN) compile-u-boot compile-linux
 	mkdir -p $(LINUX_OBJDIR)
 	cd $(LINUX_OBJDIR) && \
 	  ln -sf $(MIPS32_NEMU) nemu && \
-	  ./nemu -b -e u-boot --block-data ddr:0x4000000:vmlinux 2>npc.out
+	  ./nemu -b -e u-boot --block-data ddr:0x4000000:vmlinux
